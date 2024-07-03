1d72245e5d39accc7e7552ea740bc8579c3c68c9
Michał Wiśniewski <michiwisniewski2001@gmail.com>
2024-07-03 11:21:10 +0200

    Update Main.java

--- WordleFinder/src/Main.java
+++ WordleFinder/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static List<String> wordList = new ArrayList<String>();
+
+    public static final String LANGUAGE = "PL";
+
+    static {
+        Utils.loadLanguage(LANGUAGE);
+    }
+
+
     public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
 
     }
 }
