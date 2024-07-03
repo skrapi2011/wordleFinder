25c13b9fd043c3d2aa4bd1fa0db9f311cc4c9b05
Michał Wiśniewski <michiwisniewski2001@gmail.com>
2024-07-03 11:21:05 +0200

    Added loadLanguage()

--- WordleFinder/src/Utils.java
+++ WordleFinder/src/Utils.java
@@ -1,15 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Utils {
 
+    /** Loads given language
+     *
+     * @param language Language to load (ex. "EN")
+     */
+    public static void loadLanguage(String language){
+        try{
+            String path = "languages/"+language+".txt";
+            Scanner sc = new Scanner(new File(path));
+            String line;
 
+            while(sc.hasNextLine()){
+                line = sc.nextLine();
+                Main.wordList.add(line);
+            }
 
-
-
-
-
-
-
-
-
+            sc.close();
+        }catch(FileNotFoundException e){
+            System.err.println("File "+language+".txt not found");
+        }
+    }
 
 
 
